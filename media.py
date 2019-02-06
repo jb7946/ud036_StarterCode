6e6795cfb60b127ada015195e1769a8f94d4b1db
jdb <jdb@jdb-mbp.attlocal.net>
2019-02-05 21:34:44 -0600

    Updating project to meet educational request.  adding
    media.py, entertainment_center.py, README.md

---
+++ media.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+class Movie():
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
+class Video():
+    def __init__(self, title, duration):
+        self.title = video.title
+        self.duration = video.duration
